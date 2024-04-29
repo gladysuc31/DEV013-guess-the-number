c771d9f99ba31a84d990e06e4c74f24350a91389
gladysuc31 <gladyslizeth31@gmail.com>
2024-04-28 23:14:32 -0500

    Refactored functions, changed names and updated tests.

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 import random
 
 #Compare guess to the correct number
-def compareNumber(secretNumber, userInput):
-    if secretNumber == userInput:
-        print("You guessed right!")
-        return True
+def compare_number(secret_number, user_input):
+    if secret_number == user_input:
+        return True, "You guessed right!"
     else:
-        if userInput < secretNumber:
-            print("Number should be higher, try again!")
-        elif userInput > secretNumber:
-            print("Number should be lower, try again!")
-        return False
+        if user_input < secret_number:
+            return False, "Number should be higher, try again!"
+        elif user_input > secret_number:
+            return False, "Number should be lower, try again!"
     
 #Get the user´s guess
-def userGuess():
-    return int(input("What is the secret number? "))
+def user_guess():
+    while True:
+        try:
+            return int(input("What is the secret number? "))
+        except ValueError:
+            print("Please enter a valid number.")
 
 
 #Get computer´s guess
-def computerGuess():
-    randomNumber = random.randint(1, 100)
-    print("Computer guessed:", randomNumber)
-    return randomNumber
+def computer_guess():
+    random_number = random.randint(1, 100)
+    print("Computer guessed:", random_number)
+    return random_number
 
 #Function to store the user´s guesses
-def storeAttempts(userAttempts, attempt):
-    userAttempts.append(attempt)
+def store_attempts(user_attempts, attempt):
+    user_attempts.append(attempt)
 
 #Game implementation
 def main():
-    secretNumber = random.randint(1, 100)
-    print(secretNumber)
-    userAttempts = []
-    computerAttempts = []
+    secret_number = random.randint(1, 100)
+    print(secret_number)
+    user_attempts = []
+    computer_attempts = []
 
     while True:
-        userInput = userGuess()
-        storeAttempts(userAttempts, userInput)
+        user_input = user_guess()
+        store_attempts(user_attempts, user_input)
         
+        result, message = compare_number(secret_number, user_input)
+        print(message)
         
-        if compareNumber(secretNumber, userInput) == True:
-            print(userAttempts)
+        if result:
+            print("User attempts: " + str(user_attempts))
             break
 
-        computerInput = computerGuess()
-        storeAttempts(computerAttempts, computerInput)
-        if compareNumber(secretNumber, computerInput) == True:
-            print(computerAttempts)
+        computer_input = computer_guess()
+        store_attempts(computer_attempts, computer_input)
+        
+        result, message = compare_number(secret_number, computer_input)
+        print(message)
+        
+        if result:
+            print("The computer attempts: " + str(computer_attempts))
             break
 
 main()
--- test_functions.py
+++ test_functions.py
@@ -1,14 +1,23 @@
 import unittest
+from unittest.mock import patch
 import main
 
 class testFunctions(unittest.TestCase):
 
-    def test_compareNumber(self):
-        self.assertFalse(main.compareNumber(3,5))
-        self.assertTrue(main.compareNumber(89,89))
+    def test_compare_number(self):
+        result, message = main.compare_number(3, 5)
+        self.assertFalse(result)
 
-    def test_userGuess(self):
-        self.assertEqual(type(main.userGuess()), int)
+    @patch('builtins.input', return_value='42')
+    def test_user_guess_returns_int(self, mock_input):
+        user_input = main.user_guess()
+        self.assertIsInstance(user_input, int)
 
-    def test_computerGuess(self):
-        self.assertLessEqual(main.computerGuess(), 101)
+    @patch('main.random.randint', return_value=42)
+    def test_computer_guess_in_range(self, mock_randint):
+        computer_input = main.computer_guess()
+        self.assertLessEqual(computer_input, 100)
+        self.assertGreaterEqual(computer_input, 1)
+
+if __name__ == '__main__':
+    unittest.main()
