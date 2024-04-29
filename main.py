907cbe29bb35be5f0dd6b75cf276bca72a2a85d7
gladysuc31 <gladyslizeth31@gmail.com>
2024-04-29 00:14:50 -0500

    Updates based on pylint suggestions.

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
+"""Main"""
 import random
 
-#Compare guess to the correct number
 def compare_number(secret_number, user_input):
+    """Compare guess to the correct number"""
     if secret_number == user_input:
         return True, "You guessed right!"
-    else:
-        if user_input < secret_number:
-            return False, "Number should be higher, try again!"
-        elif user_input > secret_number:
-            return False, "Number should be lower, try again!"
-    
-#Get the user´s guess
+    if user_input < secret_number:
+        return False, "Number should be higher, try again!"
+    if user_input > secret_number:
+        return False, "Number should be lower, try again!"
+
 def user_guess():
+    """Get the user´s guess"""
     while True:
         try:
             return int(input("What is the secret number? "))
         except ValueError:
             print("Please enter a valid number.")
 
-
-#Get computer´s guess
 def computer_guess():
+    """Get computer´s guess"""
     random_number = random.randint(1, 100)
     print("Computer guessed:", random_number)
     return random_number
 
-#Function to store the user´s guesses
+
 def store_attempts(user_attempts, attempt):
+    """Function to store the user´s guesses"""
     user_attempts.append(attempt)
 
-#Game implementation
+
 def main():
+    """Game implementation"""
     secret_number = random.randint(1, 100)
     print(secret_number)
     user_attempts = []
@@ -39,17 +40,16 @@ def main():
     while True:
         user_input = user_guess()
         store_attempts(user_attempts, user_input)
-        
+
         result, message = compare_number(secret_number, user_input)
         print(message)
-        
+
         if result:
             print("User attempts: " + str(user_attempts))
             break
 
         computer_input = computer_guess()
         store_attempts(computer_attempts, computer_input)
-        
         result, message = compare_number(secret_number, computer_input)
         print(message)
         
@@ -58,3 +58,4 @@ def main():
             break
 
 main()
+#End-of-file (EOF)
--- test_functions.py
+++ test_functions.py
@@ -1,20 +1,25 @@
+"""Tests"""
 import unittest
 from unittest.mock import patch
 import main
 
-class testFunctions(unittest.TestCase):
-
+class TestFunctions(unittest.TestCase):
+    """tests for main.py functions"""
+    
     def test_compare_number(self):
+        """test to check if the result is false"""
         result, message = main.compare_number(3, 5)
         self.assertFalse(result)
 
     @patch('builtins.input', return_value='42')
     def test_user_guess_returns_int(self, mock_input):
+        """test to verify the user's input is an interger"""
         user_input = main.user_guess()
         self.assertIsInstance(user_input, int)
 
     @patch('main.random.randint', return_value=42)
     def test_computer_guess_in_range(self, mock_randint):
+        """test to verify the random interger is between 1-100"""
         computer_input = main.computer_guess()
         self.assertLessEqual(computer_input, 100)
         self.assertGreaterEqual(computer_input, 1)
